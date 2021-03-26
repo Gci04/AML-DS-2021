b0bb9a3b6be8ca05e6d580e299c63ef2d13a80cd
Gci04 <gcinzoe04@gmail.com>
2021-03-26 16:27:29 +0300

    Change to classification task

--- scripts/getData.py
+++ scripts/getData.py
@@ -3,24 +3,30 @@ import pandas as pd
 import numpy as np
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 def get_data(data_path="../data/SeoulBikeData.csv",testData = False):
     assert os.path.isfile(data_path), f"{os.path.realpath(data_path)} : File not exist"
 
     df = pd.read_csv(data_path)
     df = df.select_dtypes(include=[np.number]) #select columns with numerical data
-    print(df.shape)
+    df["target"] = df["Rented Bike Count"].apply(lambda x : 1 if  x > 500 else 0)
+    df.drop(["Rented Bike Count"],axis=1,inplace=True)
 
     if testData :
-        xtrain, xtest, ytrain, ytest = train_test_split(df.drop(["Rented Bike Count"],axis=1).values, df["Rented Bike Count"].values,test_size=0.20, random_state=42)
-        return DataLoader(TensorDataset(torch.from_numpy(xtrain),torch.from_numpy(ytrain)), batch_size=30), DataLoader(TensorDataset(torch.from_numpy(xtest),torch.from_numpy(ytest)))
+        xtrain, xtest, ytrain, ytest = train_test_split(df.drop(["target"],axis=1).values, df["target"].values.reshape(-1,1),test_size=0.20, random_state=42)
+        scaler = StandardScaler().fit(xtrain)
+        xtrain = torch.from_numpy(scaler.transform(xtrain)).float()
+        xtest = torch.from_numpy(scaler.transform(xtest)).float()
+        return DataLoader(TensorDataset(xtrain,torch.from_numpy(ytrain).float()), batch_size=30), DataLoader(TensorDataset(xtest,torch.from_numpy(ytest).float()))
 
-    y = torch.from_numpy(df["Rented Bike Count"].values).float()
-    x = torch.from_numpy(df.drop(["Rented Bike Count"],axis=1).values).float()
+    scaler = StandardScaler().fit(df.drop(["target"],axis=1).values)
+    x = scaler.transform(df.drop(["target"],axis=1).values)
+    y = torch.from_numpy(df["target"].values.reshape(-1,1)).float()
+    x = torch.from_numpy(x).float()
 
     return DataLoader(TensorDataset(x,y), batch_size=30)
 
-_,_ = get_data(testData=True)
 
-# if __name__ == '__main__':
-#     get_train_data()
+if __name__ == '__main__':
+    # _,_ = get_data(testData=True)
--- src/train.py
+++ src/train.py
@@ -8,30 +8,30 @@ import scripts
 device = 'cuda' if torch.cuda.is_available() else 'cpu' # get device for training model
 writer = SummaryWriter()
 
-class NeuralNetworkRegression(nn.Module):
+class NeuralNetworkClassification(nn.Module):
     def __init__(self,input_dim=10,output_dim=1):
-        super(NeuralNetworkRegression, self).__init__()
+        super(NeuralNetworkClassification, self).__init__()
         self.layer1 = nn.Linear(input_dim, 50)
         self.layer2 = nn.Linear(50, 25)
         self.output = nn.Linear(25, output_dim)
 
     def forward(self, x):
         x = torch.tanh(self.layer1(x))
-        x = torch.tanh(self.layer2(x))
-        x = self.output(x)
+        x = torch.relu(self.layer2(x))
+        x = torch.sigmoid(self.output(x))
         return x
 
 def main(args):
 
     #load data
-    train_DataLoader = scripts.get_data(data_path="../data/SeoulBikeData.csv",testData = False)
+    train_DataLoader, _  = scripts.get_data(data_path="../data/SeoulBikeData.csv",testData = True)
     # train_DataLoader = DataLoader(TensorDataset(train_x, train_y), batch_size=30)
 
-    model = NeuralNetworkRegression(input_dim=9).float()
+    model = NeuralNetworkClassification(input_dim=9).float()
 
     lr = 1e-1
     n_epochs = 100
-    loss_fn = nn.MSELoss(reduction='mean')
+    loss_fn = nn.BCELoss(reduction='mean')
     optimizer = optim.SGD(model.parameters(), lr=lr)
 
     size = len(train_DataLoader.dataset)
@@ -48,6 +48,8 @@ def main(args):
 
             l += (loss/size)
 
+        if (epoch+1) % 10 == 0:
+            print("Epoch : {}, loss : {:.5f}".format(epoch+1,l))
         writer.add_scalar("train_loss", l, epoch)
         for tag, parm in model.named_parameters():
             writer.add_histogram(tag, parm.grad.data.cpu().numpy(), epoch)
