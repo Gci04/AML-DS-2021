0fa085f8839164f342d9c23f45c782a0a43b218f
quarriedstone <quarriedstone@gmail.com>
2021-04-15 02:04:00 +0300

    feature: added HW1 scripts for training

--- scripts/getData.py
+++ scripts/getData.py
@@ -5,29 +5,25 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-def get_data(data_path="../data/SeoulBikeData.csv",testData = False):
-    assert os.path.isfile(data_path), f"{os.path.realpath(data_path)} : File not exist"
 
-    df = pd.read_csv(data_path, engine='python')
-    df = df.select_dtypes(include=[np.number]) #select columns with numerical data
-    df["target"] = df["Rented Bike Count"].apply(lambda x : 1 if  x > 500 else 0)
-    df.drop(["Rented Bike Count"],axis=1,inplace=True)
+def get_data_name(path):
+    def padding(char_list):
+        char_list.extend([0] * (embedding_num - len(char_list)))
+        return char_list
 
-    if testData :
-        xtrain, ytrain = df.drop(["target"],axis=1).values[:7008], df["target"].values[:7008].reshape(-1,1)
-        xtest, ytest = df.drop(["target"],axis=1).values[7008:], df["target"].values[7008:].reshape(-1,1)
-        # xtrain, xtest, ytrain, ytest = train_test_split(df.drop(["target"],axis=1).values[7008], df["target"].values.reshape(-1,1),test_size=0.20, random_state=42)
-        scaler = StandardScaler().fit(xtrain)
-        xtrain = torch.from_numpy(scaler.transform(xtrain)).float()
-        xtest = torch.from_numpy(scaler.transform(xtest)).float()
-        return DataLoader(TensorDataset(xtrain,torch.from_numpy(ytrain).float()), batch_size=30), DataLoader(TensorDataset(xtest,torch.from_numpy(ytest).float()))
+    data = pd.read_csv(path)
+    data.sort_values(by="Name", key=lambda col: col.apply(lambda x: len(x)), inplace=True)
+    unique = list(set("".join(data["Name"])))
+    unique.sort()
+    vocab = dict(zip(unique, range(1, len(unique) + 1)))
 
-    scaler = StandardScaler().fit(df.drop(["target"],axis=1).values)
-    x = scaler.transform(df.drop(["target"],axis=1).values)
-    y = torch.from_numpy(df["target"].values.reshape(-1,1)).float()
-    x = torch.from_numpy(x).float()
+    # Get maximum number of characters
+    embedding_num = data["Name"].apply(lambda x: len(x)).max()
 
-    return DataLoader(TensorDataset(x,y), batch_size=30)
+    data["Name"] = data["Name"].apply(lambda string: padding([vocab[x] for x in list(string)]))
+    # One is M and Zero is F
+    data["Gender"] = data["Gender"].apply(lambda x: 1 if x == "M" else 0)
+    return data, vocab
 
 
 if __name__ == '__main__':
--- src/test.py
+++ src/test.py
@@ -1,46 +1,90 @@
-from context import scripts
-import scripts
 from torch import nn
 import torch
-import numpy as np
-from sklearn.metrics import accuracy_score
+import torch.utils.data as data_utils
+
+from scripts import get_data_name, accuracy
+
 
 class NeuralNetworkClassification(nn.Module):
-    def __init__(self,input_dim=10,output_dim=1):
+    def __init__(self, vocab_len, input_dim=15, output_dim=1):
         super(NeuralNetworkClassification, self).__init__()
-        self.layer1 = nn.Linear(input_dim, 50)
-        self.layer2 = nn.Linear(50, 25)
+        self.embedding = nn.Embedding(vocab_len + 1, input_dim)
+        self.flatten = nn.Flatten()
+        self.layer1 = nn.Linear(input_dim ** 2, 25)
         self.output = nn.Linear(25, output_dim)
 
     def forward(self, x):
-        x = torch.tanh(self.layer1(x))
-        x = torch.relu(self.layer2(x))
-        x = torch.sigmoid(self.output(x))
+        x = self.flatten(self.embedding(x))
+        x = torch.relu(self.layer1(x))
+        x = self.output(x)
         return x
 
-# evaluate the model
-def evaluate_model(test_dl, model):
-    predictions, actuals = list(), list()
-    for inputs, targets in test_dl:
-        yhat = model(inputs)
-        yhat = yhat.detach().numpy()
-        actual = targets.numpy()
-        actual = actual.reshape((len(actual), 1))
-        # round to class values
-        yhat = yhat.round()
-        predictions.append(yhat)
-        actuals.append(actual)
-    predictions, actuals = np.vstack(predictions), np.vstack(actuals)
-    # calculate accuracy
-    acc = accuracy_score(actuals, predictions)
-    return acc
+
+class BaselineLSTM(nn.Module):
+    def __init__(self, vocab_len, embedding_dim=5, input_dim=15, output_dim=1):
+        super(BaselineLSTM, self).__init__()
+        self.embedding = nn.Embedding(num_embeddings=vocab_len + 1, embedding_dim=embedding_dim)
+        self.lstm_layer = nn.LSTM(input_size=embedding_dim,
+                                  hidden_size=5,
+                                  num_layers=1,
+                                  batch_first=True,
+                                  bidirectional=False)
+        self.flatten = nn.Flatten()
+        self.linear = nn.Linear(input_dim * embedding_dim, output_dim)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x, _ = self.lstm_layer(x)
+        x = self.flatten(x)
+        x = self.linear(x)
+        return x
+
+
+class BetterLSTM(nn.Module):
+    def __init__(self, vocab_len, embedding_dim=5, input_dim=15, output_dim=1):
+        super(BetterLSTM, self).__init__()
+        self.embedding = nn.Embedding(num_embeddings=vocab_len + 1, embedding_dim=embedding_dim)
+        self.lstm_layer = nn.LSTM(input_size=embedding_dim,
+                                  hidden_size=5,
+                                  num_layers=1,
+                                  batch_first=True,
+                                  bidirectional=False)
+        self.flatten = nn.Flatten()
+        self.linear1 = nn.Linear(input_dim * embedding_dim, 20)
+        self.linear2 = nn.Linear(20, output_dim)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x, _ = self.lstm_layer(x)
+        x = self.flatten(x)
+        x = torch.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
 
 
 if __name__ == '__main__':
-    _,testDataLoader = scripts.get_data(data_path="../data/SeoulBikeData.csv",testData = True)
-    model = NeuralNetworkClassification(input_dim=9)
-    model.load_state_dict(torch.load("./trained_model.pth"))
+    _, vocab = get_data_name("../data/data/train_eng.csv")
+    test_data, _ = get_data_name("../data/data/test_eng.csv")
 
-    model.eval()
-    model_acc = evaluate_model(testDataLoader,model)
-    print("Model Accuracy : {:.1f}%".format(model_acc*100))
+    target_test = torch.tensor(test_data["Gender"].values).to(torch.long)
+    features_test = torch.Tensor(list(test_data["Name"].values)).to(torch.long)
+    test = data_utils.TensorDataset(features_test, target_test)
+    test_loader = data_utils.DataLoader(test)
+
+    model_classical = NeuralNetworkClassification(len(vocab)).float()
+    model_baseline = BaselineLSTM(len(vocab)).float()
+    model_better_baseline = BetterLSTM(len(vocab)).float()
+
+    model_classical.load_state_dict(torch.load("./model_classical.pth"))
+    model_baseline.load_state_dict(torch.load("./model_baseline.pth"))
+    model_better_baseline.load_state_dict(torch.load("./model_better_baseline.pth"))
+    model_classical.eval()
+    model_baseline.eval()
+    model_better_baseline.eval()
+
+    model_classical_acc = accuracy(model_classical, test_loader)
+    model_baseline_acc = accuracy(model_baseline, test_loader)
+    model_better_baseline_acc = accuracy(model_better_baseline, test_loader)
+    print(f"Model Baseline Accuracy: {round(model_baseline_acc, 2)}%")
+    print(f"Model Better LSTM Accuracy: {round(model_better_baseline_acc, 2)}%")
+    print(f"Model Classical Accuracy: {round(model_classical_acc, 2)}%")
